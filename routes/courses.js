57b1293ea7b71950e54acd61655f0b325df4afdf
Vuk Stojkovic <vukbg.stojkovic@gmail.com>
2022-03-14 23:01:09 +0100

    protected routes according to role of user

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,7 @@ const {getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require(
 const Course = require('../models/course');
 const advancedResults = require('../middleware/advancedResults');
 const router = express.Router({mergeParams: true});
-const {protect} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 
 
 router
@@ -14,13 +14,13 @@ router
         select: 'name description'
     }),
     getCourses)
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse);
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 
 module.exports = router;
